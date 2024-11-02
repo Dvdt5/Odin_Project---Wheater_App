8f27aedaaa792a08112f93db477fc708f2d24f3e
Davut Dagtekin <dagtekindavut07@gmail.com>
2024-11-02 13:51:14 +0300

    made the styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+// async function getWheater () {
+//     const apiData = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Turkey?unitGroup=us&key=BXLX3MCDLNNBRR288CBDJG85N&contentType=json");
+
+//     const data = await apiData.json();
+//     console.log(data);
+// }
+
+// getWheater();
